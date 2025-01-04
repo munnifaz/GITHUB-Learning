4b69e1e77c7c9146ff7257e8b1a16a7856923540
unknown <pratibhadagar043.com>
2025-01-05 00:05:58 +0530

    Functions commit

---
+++ Funstions.java
@@ -0,0 +1,13 @@
+public class Funstions {
+    public static void sum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+   
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+        sum(a, b); // Function Call
+ 
+    }
+}
