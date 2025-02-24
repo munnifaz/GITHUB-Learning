04b2ae7bad5a77c5e8ba00f85f9cfc5f1d278136
AndyAtmadja03S <atmandy345@gmail.com>
2025-02-24 19:46:06 +1100

    Add logo

--- ta/src/components/Body.tsx
+++ ta/src/components/Body.tsx
@@ -1,14 +1,22 @@
 import data from '../data/data'
+import filterlist from '../assets/filterlist.png'
+import filteralt from '../assets/fileteralt.png'
 
 function Body() {
     return (
         <div className="flex flex-col bg-white w-full h-full overflow-y-scroll">
             <div className='flex justify-center bg-white w-full'>
-                <div className="flex flex-row w-full max-w-[97%] justify-between mt-4">
+                <div className="flex flex-row w-full max-w-[97%] justify-between mt-4 gap-x-4">
                     <div className="flex flex-row">
-                        <div className="flex gap-2 justify-content items-center border-[2px] border-orange-500 h-[44px] w-[134px] bg-white rounded-xl cursor-pointer"></div>
+                        <div className="flex justify-start px-4 gap-2 items-center border-[2px] border-orange-500 h-[44px] w-[134px] bg-white rounded-xl cursor-pointer">
+                            <img src={filteralt} className='flex w-6'>
+                            </img>
+                            <p className='flex text-orange-500 font-bold'>
+                                Filters 
+                            </p>
+                        </div>
                     </div>
-                    <div className="flex flex-row gap-2 items-start border border-gray-400 rounded-md bg-white px-2 py-2 h-[44px] w-full max-w-[800px] hover:border-black">
+                    <div className="flex flex-row items-start border border-gray-400 rounded-md bg-white px-2 py-2 h-[44px] w-full max-w-[800px] hover:border-black">
                         <input
                             type="text"
                             placeholder="Search for a building..."
@@ -16,7 +24,13 @@ function Body() {
                         />
                     </div>
                     <div>
-                        <div className="flex gap-2 justify-content items-center border-[2px] border-orange-500 h-[44px] w-[120px] bg-white rounded-xl cursor-pointer"></div>
+                        <div className="flex gap-2 justify-start px-4 items-center border-[2px] border-orange-500 h-[44px] w-[120px] bg-white rounded-xl cursor-pointer">
+                        <img src={filterlist} className='flex w-6'>
+                            </img>
+                            <p className='flex text-orange-500 font-bold'>
+                                Sort
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- ta/src/components/Header.tsx
+++ ta/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import closed from '../assets/freeroomsDoorClosed.png'
 import freerooms from '../assets/freeRoomsLogo.png'
 import title from '../assets/title.png'
-
+import darkmode from '../assets/darkmode.png'
+import grid from '../assets/grid.png'
+import search from '../assets/search.png'
+import map from '../assets/map.png'
 import { useState } from 'react';
 
 function Header() {
@@ -18,17 +21,27 @@ function Header() {
                             className='w-[48px] h-[50px] cursor-pointer' 
                             // update state every on click to alter
                             onClick={() => setClosedDoor(!closedDoor)}
-                            alt="FreeRooms Logo"
                         />
-                        <img src={title} className='w-[75%] h-[37px] mt-2' />
+                        <img src={title} className='w-[75%] h-[37px] mt-2 max-[600px]:hidden' />
                     </div>
                        
                     <div className='flex flex-row mb-1 gap-[7px]'>
-                        <div className="flex gap-2 justify-content items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer"></div>
-                        <div className="flex gap-2 justify-content items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer"></div>
-                        <div className="flex gap-2 justify-content items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer"></div>
-                        <div className="flex gap-2 justify-content items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer"></div>
-                        <div className="flex gap-2 justify-content items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer"></div>
+                        <div className="flex gap-2 justify-center items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer">
+                            <img src={search} className="flex w-6">
+                            </img>
+                        </div>
+                        <div className="flex gap-2 justify-center items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer">
+                            <img src={grid} className="flex w-6">
+                            </img>
+                        </div>
+                        <div className="flex gap-2 justify-center items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer">
+                            <img src={map} className="flex w-6">
+                            </img>
+                        </div>
+                        <div className="flex gap-2 justify-center items-center border-[1px] border-orange-300 h-[40px] w-[40px] bg-white rounded-md hover:bg-black/5 hover:border-orange-500 transition cursor-pointer">
+                            <img src={darkmode} className="flex w-6">
+                            </img>
+                        </div>
                     </div>
                 </div>
             </div>
