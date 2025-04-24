4eda12cc8d5516a53921e77a25009092a9b77532
SamAkinbola <akinbolaadebobola@gmail.com>
2025-04-24 10:41:50 +0100

    modified

--- Finance/V5.5.6__Error_Table.sql
+++ Finance/V5.5.6__Error_Table.sql
@@ -1,6 +1,6 @@
 -- Create a table named "products" with columns for product_id, name, category, and price
 CREATE TABLE OPERATION.PUBLIC.products (
-    product_id INT  -- Syntax error: missing comma after 'INT'
+    product_id INT,  -- Syntax error: missing comma after 'INT'
     name VARCHAR(255),
     category VARCHAR(255),
     price DECIMAL(10, 2)
@@ -10,4 +10,4 @@ CREATE TABLE OPERATION.PUBLIC.products (
 INSERT  products (product_id, name, category, price) VALUES
     (1, 'Laptop', 'Electronics', 1200.00),
     (2, 'Mouse', 'Electronics', 25.00),
-    (3, 'Keyboard', 'Electronics' 50.00);  -- Syntax error: missing comma after 'Electronics'
+    (3, 'Keyboard', 'Electronics', 50.00);  -- Syntax error: missing comma after 'Electronics'
