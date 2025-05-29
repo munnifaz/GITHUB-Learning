31076cb180c87573a5910dec264b17750b4358e2
Denis Gimaldinov <d.gimaldinov@spb.tpgtaipit.ru>
2025-05-29 09:17:01 +0300

    FIX HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        printMessage("Hello!");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
     }
 }
